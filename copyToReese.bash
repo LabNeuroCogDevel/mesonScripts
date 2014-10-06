d6ee1e8330f59ce0f6cc96a5970b01058069d9eb
Will Foran <willforan@gmail.com>
2014-10-06 13:47:01 -0400

    auto update 2014-10-06

--- copyToReese.bash
+++ copyToReese.bash
@@ -2,6 +2,8 @@
 message="auto update $(date +%F)"
 [ -n "$1" ] && message=$1;
 
+cd $(dirname $0)
+
 crontab -l > meson.crontab
 rsync *bash *rsync *md *crontab reese:~/src/mesonScripts/
 ssh reese "cd ~/src/mesonScripts; git diff --exit-code >/dev/null || (git commit -am '$message'; git push)"
