fbd20d9f6863074cc940a889cfc11093204995ca
Will Foran <willforan@gmail.com>
2015-04-27 04:53:06 -0400

    auto update 2015-04-27

--- 00_rsync.bash
+++ 00_rsync.bash
@@ -4,6 +4,6 @@ remote=foranw@wallace:/data/Luna1/Raw
 
 rsync $opts /home/kaihwang/MRlinks/P5Sz/ $remote/P5Sz/ |tee Sz.rsync
 rsync $opts /home/kaihwang/MRlinks/MultiModal/ $remote/MultiModal/ |tee MM.rsync
-rsync $opts /home/kaihwang/MRlinks/Reward_Sz_MRCTR/ $remote/MRCTR/ |tee Rew.rsync
+rsync $opts /home/kaihwang/MRlinks/Reward/ $remote/MRCTR/ |tee Rew.rsync
 rsync $opts /home/kaihwang/MRlinks/WorkingMemory/ $remote/WorkingMemory/ |tee WM.rsync
 
